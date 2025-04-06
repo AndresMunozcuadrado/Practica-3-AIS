a639e897001ac017bcc41caf9c740bcf5e2df1b8
raulsnchz <r.sanchezb.2022@alumnos.urjc.es>
2025-04-06 16:47:54 +0200

    refactorizaciones intermedias 13 a 20

--- AIS-Practica-3-2025-enunciado/src/test/java/es/codeurjc/test/CalculatorParserTest.java
+++ AIS-Practica-3-2025-enunciado/src/test/java/es/codeurjc/test/CalculatorParserTest.java
@@ -15,12 +15,37 @@ public class CalculatorParserTest {
     }
 
     @Test
-    public void test4a6(){
+    public void test4a7(){
         CalculatorParser parser = new CalculatorParser();
         assertEquals(parser.parse("1+1"), 2);
         assertEquals(parser.parse("2+3"), 5);
         assertEquals(parser.parse("2+3+4"), 9);
         assertEquals(parser.parse("1+2+3+4"), 10);
     }
-    
+
+    public String aTAux(String expression){
+        CalculatorParser parser = new CalculatorParser();
+        IllegalArgumentException result = assertThrows(IllegalArgumentException.class, () -> parser.parse(expression));
+        return result.getMessage();
+    }
+    @Test
+    public void test8a12(){
+        assertEquals("Invalid expression", aTAux("A"));
+        assertEquals("Invalid expression", aTAux("B"));
+        assertEquals("Invalid expression", aTAux("k"));
+        assertEquals("Invalid expression", aTAux("HoLa"));
+        assertEquals("Invalid expression", aTAux("1 + A"));
+    }
+    @Test
+    public void test13a20(){
+        CalculatorParser parser = new CalculatorParser();
+        assertEquals(parser.parse("5-3"), 2);
+        assertEquals(parser.parse("1-2"), -1);
+        assertEquals(parser.parse("7-2-1"), 4);
+        assertEquals(parser.parse("9-5-3-1"), 0);
+        assertEquals(parser.parse("7+1-5"), 3);
+        assertEquals(parser.parse("9-5+4"), 8);
+        assertEquals(parser.parse("9+1-6-2"), 2);
+        assertEquals(parser.parse("-5+9"), 4);
+    }
 }
